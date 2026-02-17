898385d0a9d523213dbbe65e56ab39365fc5312b
Lia <aliyahrenomeron7@gmail.com>
2026-02-16 19:42:51 -0500

    added comments and fixed white spacing

--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
-
 document.addEventListener("DOMContentLoaded", () => {
-    buttonWorks();
-})
+  buttonWorks();
+});
 
 function buttonWorks() {
-    let start = document.getElementById("start-btn");
+  // Storing the start button in a variable
+  let start = document.getElementById("start-btn");
 
-    start.addEventListener("click", mouseClick);
-    
-    function mouseClick() {
-        console.log("START BTN CLICKED");
+  // Creates a listener event to tell the DOM to look out for mouse clicks
+  start.addEventListener("click", mouseClick);
 
-        window.location.href = "/html/newPage.html"
-    }
+  // Mouse Click function, storing the info on where to go when the start button is clicked
+  function mouseClick() {
+    console.log("START BTN CLICKED");
+
+    window.location.href = "/html/newPage.html";
+  }
 }
